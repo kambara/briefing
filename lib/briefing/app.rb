b37cc38f149874f28aa36d4b38500e463fd866e3
kambara <kambara@sappari.org>
2012-12-07 02:15:24 +0900

    Add a feature to use local images in slides

--- lib/briefing/app.rb
+++ lib/briefing/app.rb
@@ -1,20 +1,31 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'briefing/slides'
+require 'pathname'
 
 module Briefing
   class App < Sinatra::Base
+    configure do
+      set :briefing_dir, Pathname(File.dirname(__FILE__))
+      set :public_folder, briefing_dir + 'public'
+      set :views, briefing_dir + 'views'
+    end
+
     configure :development do
       register Sinatra::Reloader
-      dir = File.dirname(__FILE__)
-      set :public_folder, File.join(dir, 'public')
-      set :views, File.join(dir, 'views')
+      also_reload(briefing_dir + 'slides.rb')
     end
 
     get '/' do
       @slides = Slides.new(settings.slides_source)
       @options = @slides.options
       erb :index
+    end
+
+    get '/assets/:file.:ext' do |file, ext|
+      content_type ext
+      slides_file = Pathname(File.join(Dir::pwd, settings.slides_source))
+      send_file(slides_file.dirname + 'assets' + "#{file}.#{ext}")
     end
   end
 end
--- lib/briefing/slides.rb
+++ lib/briefing/slides.rb
@@ -1,10 +1,43 @@
 require 'yaml'
+require 'cgi'
 
 module Briefing
   class Slides
     attr_reader :sections, :options
+    
+    def initialize(filepath)
+      @sections = []
+      buffer = []
+      File.open(filepath).read.each_line {|line|
+        if line.match /^-{3,}(.*)$/
+          @sections << buffer.join('') if buffer.length > 0
+          buffer = []
+        else
+          buffer << modify_assets_path(line)
+        end
+      }
+      @sections << buffer.join('') if buffer.length > 0
+      @options = Options.new(@sections.shift) if @sections.length > 0
+    end
 
-    DEFAULTS = {
+    def modify_assets_path(line)
+      ## ![](hoge.jpg)  =>  ![](assets/hoge.jpg)
+      line.gsub(/!\[(.*)\]\((.+)\)/) {|matched|
+        alt = $1
+        path = $2
+        if path.match /^https?:\/\//
+          matched
+        elsif path.match /^assets\//
+          matched
+        else
+          "![#{alt}](assets/#{CGI.escape(path)})"
+        end
+      }
+    end
+  end
+
+  class Options < Hash
+    REVEALJS_DEFAULTS = {
       'controls'     => false,
       'progress'     => true,
       'history'      => true,
@@ -18,23 +51,18 @@ module Briefing
       'rollingLinks' => false
     }
 
-    def initialize(filepath)
-      @sections = []
-      buffer = []
-      File.open(filepath).read.each_line {|line|
-        if line.match /^-{3,}(.*)$/
-          if buffer.length > 0
-            @sections << buffer.join("")
-          end
-          buffer = []
-        else
-          buffer << line
-        end
-      }
-      if buffer.length > 0
-        @sections << buffer.join("")
+    def initialize(yaml_text)
+      if options = YAML.load(yaml_text)
+        self.merge! options
       end
-      @options = DEFAULTS.merge(YAML.load(@sections.shift))
+    end
+
+    def revealjs
+      options = {}
+      REVEALJS_DEFAULTS.each_pair {|key, value|
+        options[key] = self[key] || value
+      }
+      options
     end
   end
 end
